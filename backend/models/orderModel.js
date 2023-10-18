f0a5d5a3df68f3414b1b476d8abb3b1b12d80f25
sooraj888 <soorajsagar888@gmail.com>
2023-10-18 23:40:23 +0530

    add: Product order model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
+    address: { type: String, required: true },
+    pinCode: {
+      type: Number,
+      required: true,
+    },
+    phoneNo: {
+      type: Number,
+      required: true,
+    },
+  },
+  orderItem: [
+    {
+      name: { type: String, required: true },
+      place: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      image: { type: String, required: true },
+      product: {
+        type: mongoose.Schema.objectId,
+        required: true,
+        ref: "Product",
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.objectId,
+    required: true,
+    ref: "User",
+  },
+  paymentInfo: {
+    id: { type: String, required: true },
+    status: { type: String, required: true },
+  },
+  payedAt: { type: Date, require: true, required: true },
+  itemPrice: { type: Number, default: 0, required: true },
+  taxPrice: { type: Number, default: 0, required: true },
+  shippingPrice: { type: Number, default: 0, required: true },
+  totalPrice: { type: Number, default: 0, required: true },
+  orderStatus: { type: String, required: true, default: "Processing" },
+  deliveredAt: Date,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
